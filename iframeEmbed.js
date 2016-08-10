0041639e1ae7a54f6977745c88d68c8812fc0c78
John Jones <jjones@adfero.com>
2016-08-10 17:41:20 -0400

    removing things

--- iframeEmbed.js
+++ iframeEmbed.js
@@ -1,4 +1,31 @@
 'use strict';
+window.embedIFrame = function(inoptions) {
+  var options = _.extend({
+    'id': null
+    'source': null,
+    'messageTarget': '*'
+  },inoptions || {});
+  var embedIFrame = document.getElementById(options.id);
+  if (embedIFrame) {
+    var myEventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+    var myEventListener = window[myEventMethod];
+    var myEventMessage = myEventMethod == "attachEvent" ? "onmessage" : "message";
+    embedIFrame.width = "100%";
+    myEventListener(myEventMessage, function (e) {
+      if (e.data && e.data.source == options.source) {
+				embedIFrame.height = e.data.height + "px";
+				embedIFrame.scrollHeight = e.data.height + "px";
+			}
+    },false);
+    setInterval(function() {
+			embedIFrame.contentWindow.postMessage({
+				'height': window.innerHeight,
+				'width': window.innerWidth,
+        'href': window.location.href
+			},options.messageTarget);
+		},100);
+  }
+}
 window.embeddedIFrame = function(inoptions) {
   var options = _.extend({
     'source': null,
