9ea87430acc0d6756a11a23d0c4e23d59efe7618
John Jones <jjones@adfero.com>
2016-08-10 17:56:01 -0400

    wrapped in func

--- iframeEmbed.js
+++ iframeEmbed.js
@@ -1,51 +1,53 @@
 'use strict';
-window.embedIFrame = function(options) {
-  options.messageTarget = options.messageTarget || '*';
-  var embedIFrame = document.getElementById(options.id);
-  if (embedIFrame) {
+(function() {
+  window.embedIFrame = function(options) {
+    options.messageTarget = options.messageTarget || '*';
+    var embedIFrame = document.getElementById(options.id);
+    if (embedIFrame) {
+      var myEventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+      var myEventListener = window[myEventMethod];
+      var myEventMessage = myEventMethod == "attachEvent" ? "onmessage" : "message";
+      embedIFrame.width = "100%";
+      myEventListener(myEventMessage, function (e) {
+        if (e.data && e.data.source == options.source) {
+  				embedIFrame.height = e.data.height + "px";
+  				embedIFrame.scrollHeight = e.data.height + "px";
+  			}
+      },false);
+      setInterval(function() {
+  			embedIFrame.contentWindow.postMessage({
+  				'height': window.innerHeight,
+  				'width': window.innerWidth,
+          'href': window.location.href
+  			},options.messageTarget);
+  		},100);
+    }
+  }
+  window.embeddedIFrame = function(options) {
+    options.messageTarget = options.messageTarget || '*';
+    var lastHeight = 0;
+    var interval = setInterval(function(){
+      var currentHeight = document.body.offsetHeight;
+      if (currentHeight != lastHeight) {
+        lastHeight = currentHeight;
+        var prevHeight = {
+          'source': options.source,
+          'height': currentHeight
+        };
+        window.parent.postMessage(prevHeight,options.messageTarget);
+      }
+    }, 100);
     var myEventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
     var myEventListener = window[myEventMethod];
     var myEventMessage = myEventMethod == "attachEvent" ? "onmessage" : "message";
-    embedIFrame.width = "100%";
     myEventListener(myEventMessage, function (e) {
-      if (e.data && e.data.source == options.source) {
-				embedIFrame.height = e.data.height + "px";
-				embedIFrame.scrollHeight = e.data.height + "px";
-			}
-    },false);
-    setInterval(function() {
-			embedIFrame.contentWindow.postMessage({
-				'height': window.innerHeight,
-				'width': window.innerWidth,
-        'href': window.location.href
-			},options.messageTarget);
-		},100);
-  }
-}
-window.embeddedIFrame = function(options) {
-  options.messageTarget = options.messageTarget || '*';
-  var lastHeight = 0;
-  var interval = setInterval(function(){
-    var currentHeight = document.body.offsetHeight;
-    if (currentHeight != lastHeight) {
-      lastHeight = currentHeight;
-      var prevHeight = {
-        'source': options.source,
-        'height': currentHeight
-      };
-      window.parent.postMessage(prevHeight,options.messageTarget);
-    }
-  }, 100);
-  var myEventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-  var myEventListener = window[myEventMethod];
-  var myEventMessage = myEventMethod == "attachEvent" ? "onmessage" : "message";
-  myEventListener(myEventMessage, function (e) {
-    if (e.data) {
-      if (options.gaDimension && ga) {
-        ga('set', options.gaDimension, e.data.href);
+      if (e.data) {
+        if (options.gaDimension && ga) {
+          ga('set', options.gaDimension, e.data.href);
+        }
+        window.iFrameParentWidth = e.data.width;
+        window.iFrameParentHeight = e.data.height;
       }
-      window.iFrameParentWidth = e.data.width;
-      window.iFrameParentHeight = e.data.height;
-    }
-  });
-}
+    });
+  }
+})();
